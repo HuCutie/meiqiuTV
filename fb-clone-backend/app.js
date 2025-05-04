f60c5c434321d32c3f6d2455d30d2752baa2f5c3
hucutie <husmallcutie@gmail.com>
2025-05-04 21:22:28 +0800

    Remove chat feature(backend)

--- fb-clone-backend/app.js
+++ fb-clone-backend/app.js
@@ -39,12 +39,9 @@ cloudinary.config({
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const postRouter = require("./routes/post");
-const chatRouter = require("./routes/chat");
-const messageRouter = require("./routes/message");
 
 // middlewares
 const errorHandlerMiddleware = require("./middleware/error-handler");
-const authorizationMiddleware = require("./middleware/authorization");
 const notFoundMiddleware = require("./middleware/not-found");
 
 app.use(xss());
@@ -57,35 +54,10 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "welcome" });
 });
 
-// socket io
-const { addUser, getUserID, getSocketID, removeUser } = require("./socket/users");
-const { createMessage, deleteMessages, deleteChat } = require("./utils/messageSocketEvents");
-
-io.on("connection", socket => {
-    io.emit("usersOnline", addUser(socket.handshake.query.id, socket.id));
-    socket.on("send message", async (message, to, chatId, id) => {
-        socket.to(getSocketID(to)).emit("receive message", message, getUserID(socket.id));
-        await createMessage({ chatId, id, message });
-    });
-    socket.on("delete chat", async (chatID, to) => {
-        socket.to(getSocketID(to)).emit("delete chat", chatID);
-        await deleteChat({ chatID });
-    });
-    socket.on("clear chat", async (chatID, to) => {
-        socket.to(getSocketID(to)).emit("clear chat", chatID);
-        await deleteMessages({ chatID });
-    });
-    socket.on("disconnect", () => {
-        io.emit("usersOnline", removeUser(socket.id));
-    });
-});
-
 // Routes for API endpoints
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
-app.use("/api/v1/chats", authorizationMiddleware, chatRouter);
-app.use("/api/v1/messages", authorizationMiddleware, messageRouter);
 
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
